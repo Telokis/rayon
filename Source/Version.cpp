bcd27a395493b8edc10ce87e008d0c392887d4f9
Telokis <ninetainedo@gmail.com>
2017-02-08 16:49:59 +0100

    Version update

--- Source/Version.cpp
+++ Source/Version.cpp
@@ -1,13 +1,19 @@
+/*******************************************************************************
+
+                         Generated file : do not edit
+
+*******************************************************************************/
+
 #include "Version.hh"
 #include <string>
 
 using namespace std;
 
-const int RAYON_BUILD_VERSION = 18;
+const int RAYON_BUILD_VERSION = 20;
 
-const char* const RAYON_COMMIT_HASH = "acf1d43";
+const char* const RAYON_COMMIT_HASH = "127d62d";
 
 const char* const RayOn::version()
 {
-  return "0.4.18 (acf1d43)";
+  return "0.4.20 (127d62d)";
 }
