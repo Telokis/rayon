65e19e68ab01538d360ebba299b3c07fe67e166a
Telokis <ninetainedo@gmail.com>
2017-02-08 19:53:47 +0100

    Updated scripts

--- scripts/setup_hooks.sh
+++ scripts/setup_hooks.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 hook="#!bin/sh\nsource ./scripts/generate_version.sh\nexit 0"
-hooks_files=(post-checkout post-merge post-commit)
+hooks_files="post-checkout post-merge post-commit"
 
 for file in $hooks_files; do
+	echo "$file"
     echo -e $hook > "../.git/hooks/$file"
 done
 
