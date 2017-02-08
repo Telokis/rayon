5f4e0ab2815b405b493b5d87d1e81a41a13e17ca
Telokis <ninetainedo@gmail.com>
2017-02-09 00:04:29 +0100

    Version changed for testing

--- scripts/handle_tags.sh
+++ scripts/handle_tags.sh
@@ -5,7 +5,7 @@ major=$(cat $filename | tr -d '\n' | sed -r 's/.*RAYON_MAJOR_VERSION ([0-9]+).*/
 minor=$(cat $filename | tr -d '\n' | sed -r 's/.*RAYON_MINOR_VERSION ([0-9]+).*/\1/')
 build=$(git rev-list HEAD | wc -l | tr -d '\n' | tr -d ' ')
 chash=$(git rev-parse --short HEAD | tr -d '\n' | tr -d ' ')
-fullversion="$major.$minor.$build-r$chash"
+fullversion="$major.$minor.$build-$chash"
 
 echo $fullversion
 
