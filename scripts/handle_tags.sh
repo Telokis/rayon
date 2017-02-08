c184dfb001649bc0d7239412f2e3eb3c7864f80e
Telokis <ninetainedo@gmail.com>
2017-02-09 00:25:35 +0100

    Variable renamed

--- scripts/handle_tags.sh
+++ scripts/handle_tags.sh
@@ -7,8 +7,8 @@ build=$(git rev-list HEAD | wc -l | tr -d '\n' | tr -d ' ')
 chash=$(git rev-parse --short HEAD | tr -d '\n' | tr -d ' ')
 fullversion="$major.$minor.$build-$chash"
 
-ddiff=$(git diff HEAD^ HEAD)
-if (echo "$ddiff" | grep -q "+"$'\x23'"define RAYON_MAJOR_VERSION") || (echo "$ddiff" | grep -q "+"$'\x23'"define RAYON_MINOR_VERSION") ; then
+diffval=$(git diff HEAD^ HEAD)
+if (echo "$diffval" | grep -q "+"$'\x23'"define RAYON_MAJOR_VERSION") || (echo "$diffval" | grep -q "+"$'\x23'"define RAYON_MINOR_VERSION") ; then
  echo "diff !"
 fi
 echo $fullversion
