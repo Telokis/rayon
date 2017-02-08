d0fffccafa41a91dd534db2ea309758ca98105ee
Telokis <ninetainedo@gmail.com>
2017-02-09 00:48:59 +0100

    Updated scripts

--- scripts/handle_tags.sh
+++ scripts/handle_tags.sh
@@ -3,14 +3,13 @@
 filename="./Include/Version.hh"
 major=$(cat $filename | tr -d '\n' | sed -r 's/.*RAYON_MAJOR_VERSION ([0-9]+).*/\1/')
 minor=$(cat $filename | tr -d '\n' | sed -r 's/.*RAYON_MINOR_VERSION ([0-9]+).*/\1/')
-build=$(git rev-list HEAD | wc -l | tr -d '\n' | tr -d ' ')
-chash=$(git rev-parse --short HEAD | tr -d '\n' | tr -d ' ')
-fullversion="$major.$minor.$build-$chash"
+
+tagname="v$major.$minor.0"
 
 diffval=$(git diff HEAD^ HEAD)
 if (echo "$diffval" | grep -q "+"$'\x23'"define RAYON_MAJOR_VERSION") || (echo "$diffval" | grep -q "+"$'\x23'"define RAYON_MINOR_VERSION") ; then
- echo "diff !"
+	echo "major or minor version changed ! Creating tag $tagname !"
+	git tag -a "$tagname" -m "Auto tagging $tagname"
 fi
-echo $fullversion
 
 exit 0
--- scripts/setup_hooks.sh
+++ scripts/setup_hooks.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 hook_version_handling="#!bin/sh\n./scripts/generate_version.sh"
-hook_version_handling_files="post-checkout post-merge post-commit"
+hook_version_handling_files="post-checkout post-merge"
 
 for file in $hook_version_handling_files; do
-    echo -e $hook_version_handling > "../.git/hooks/$file"
+    echo -e $hook_version_handling >> "../.git/hooks/$file"
 done
 
-echo -e "./scripts/handle_tags.sh" >> "../.git/hooks/post-commit"
+echo -e "#!bin/sh\n./scripts/handle_tags.sh\n./scripts/generate_version.sh" > "../.git/hooks/post-commit"
 
 cd ..
 ./scripts/generate_version.sh
