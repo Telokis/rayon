d2ff5d8fdb3011442786b717da63a0c0ebe4b0b4
Telokis <ninetainedo@gmail.com>
2017-02-09 00:58:28 +0100

    Bumped minor version

--- Include/Version.hh
+++ Include/Version.hh
@@ -4,7 +4,7 @@
 #define RAYON_VERSION_HH_
 
 #define RAYON_MAJOR_VERSION 0
-#define RAYON_MINOR_VERSION 6
+#define RAYON_MINOR_VERSION 7
 
 extern const int RAYON_BUILD_VERSION;
 extern const char* const RAYON_COMMIT_HASH;
