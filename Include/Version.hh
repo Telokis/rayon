f6034427af0aa13b5e11b90da3c7ecbf22ff1d22
Telokis <ninetainedo@gmail.com>
2017-02-09 00:25:56 +0100

    test

--- Include/Version.hh
+++ Include/Version.hh
@@ -4,7 +4,7 @@
 #define RAYON_VERSION_HH_
 
 #define RAYON_MAJOR_VERSION 0
-#define RAYON_MINOR_VERSION 4
+#define RAYON_MINOR_VERSION 5
 
 extern const int RAYON_BUILD_VERSION;
 extern const char* const RAYON_COMMIT_HASH;
