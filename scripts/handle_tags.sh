d6ace6b425aadb981765a03daf24d33257aaec84
Telokis <ninetainedo@gmail.com>
2017-02-09 00:01:50 +0100

    Added script to auto handle tags

---
+++ scripts/handle_tags.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+filename="./Include/Version.hh"
+major=$(cat $filename | tr -d '\n' | sed -r 's/.*RAYON_MAJOR_VERSION ([0-9]+).*/\1/')
+minor=$(cat $filename | tr -d '\n' | sed -r 's/.*RAYON_MINOR_VERSION ([0-9]+).*/\1/')
+build=$(git rev-list HEAD | wc -l | tr -d '\n' | tr -d ' ')
+chash=$(git rev-parse --short HEAD | tr -d '\n' | tr -d ' ')
+fullversion="$major.$minor.$build-r$chash"
+
+echo $fullversion
+
+exit 0
--- scripts/setup_hooks.sh
+++ scripts/setup_hooks.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-hook="#!bin/sh\nsource ./scripts/generate_version.sh\nexit 0"
-hooks_files="post-checkout post-merge post-commit"
+hook_version_handling="#!bin/sh\n./scripts/generate_version.sh"
+hook_version_handling_files="post-checkout post-merge post-commit"
 
-for file in $hooks_files; do
-	echo "$file"
-    echo -e $hook > "../.git/hooks/$file"
+for file in $hook_version_handling_files; do
+    echo -e $hook_version_handling > "../.git/hooks/$file"
 done
+
+echo -e "./scripts/handle_tags.sh" >> "../.git/hooks/post-commit"
 
 cd ..
 ./scripts/generate_version.sh
