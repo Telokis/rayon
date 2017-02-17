5b579365946fb48dcc55da136996122f24cdd0fb
Telokis <ninetainedo@gmail.com>
2017-02-17 23:57:38 +0100

    Fix

--- scripts/setup_hooks.sh
+++ scripts/setup_hooks.sh
@@ -5,16 +5,19 @@ if [ "${PWD##*/}" != "scripts" ] ; then
     exit 1
 fi
 
-hook_version_handling="#!bin/bash\n./scripts/generate_version.sh"
+hook_version_handling="#!/bin/bash\n./scripts/generate_version.sh"
 hook_version_handling_files="post-checkout post-merge"
 
 for file in $hook_version_handling_files; do
     echo -e $hook_version_handling >> "../.git/hooks/$file"
+    chmod +x ../.git/hooks/$file
 done
 
-echo -e "#!bin/bash\n./scripts/handle_tags.sh\n./scripts/generate_version.sh" > "../.git/hooks/post-commit"
+echo -e "#!/bin/bash\n./scripts/handle_tags.sh\n./scripts/generate_version.sh" > "../.git/hooks/post-commit"
+chmod +x ../.git/hooks/post-commit
 
-echo -e "#!bin/bash\n./scripts/setup_readme_version.sh" > "../.git/hooks/pre-commit"
+echo -e "#!/bin/bash\n./scripts/setup_readme_version.sh" > "../.git/hooks/pre-commit"
+chmod +x ../.git/hooks/pre-commit
 
 cd ..
 ./scripts/generate_version.sh
