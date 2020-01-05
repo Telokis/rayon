2fcb6f870fb84a040fcda7a0d597786abc53e390
Telokis <telokis0@gmail.com>
2020-01-05 22:02:11 +0100

    Proper UI and theme

--- Projects/GUI/Source/MainWindow.cpp
+++ Projects/GUI/Source/MainWindow.cpp
@@ -1,15 +1,42 @@
 #include "MainWindow.hh"
-#include "./ui_MainWindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+#include <QComboBox>
+
+#include "./ui_MainWindow.h"
+#include "Color.hh"
+#include "Material.hh"
+
+void populateColors(QComboBox* colorComboBox)
 {
-    ui->setupUi(this);
+  for (auto&& colors : Rayon::colors())
+  {
+    colorComboBox->addItem(colors.first.c_str());
+  }
+}
+
+void populateFlags(QLayout* parentLayout, QWidget* parent)
+{
+  for (auto&& flags : Rayon::Material::strToFlag)
+  {
+    auto* checkBox = new QCheckBox(parent);
+
+    checkBox->setObjectName(flags.first.c_str() + QString::fromUtf8("CheckBox"));
+    checkBox->setText(flags.first.c_str());
+    parentLayout->addWidget(checkBox);
+  }
+}
+
+MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWindow)
+{
+  ui->setupUi(this);
+  populateColors(ui->colorComboBox);
+  populateFlags(ui->flagsGroupLayout, ui->flagsGroupBox);
+
+  delete ui->placeHolderCheckBox;
+  ui->placeHolderCheckBox = nullptr;
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+  delete ui;
 }
-
--- Projects/GUI/Source/main.cpp
+++ Projects/GUI/Source/main.cpp
@@ -1,11 +1,43 @@
+#include <QApplication>
+#include <QFile>
+#include <QStyleFactory>
+
 #include "MainWindow.hh"
 
-#include <QApplication>
-
-int main(int argc, char *argv[])
+void setPalette(QApplication& app)
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+  QPalette darkPalette;
+  darkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
+  darkPalette.setColor(QPalette::WindowText, Qt::white);
+  darkPalette.setColor(QPalette::Base, QColor(25, 25, 25));
+  darkPalette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
+  darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+  darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+  darkPalette.setColor(QPalette::Text, Qt::white);
+  darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
+  darkPalette.setColor(QPalette::ButtonText, Qt::white);
+  darkPalette.setColor(QPalette::BrightText, Qt::red);
+  darkPalette.setColor(QPalette::Link, QColor(0x6B, 0x23, 0x8E));
+
+  darkPalette.setColor(QPalette::Highlight, QColor(0x6B, 0x23, 0x8E));
+  darkPalette.setColor(QPalette::HighlightedText, Qt::white);
+
+  app.setPalette(darkPalette);
+
+  QFile sheet(":/style/style/main.qss");
+  sheet.open(QFile::ReadOnly);
+  QString style(sheet.readAll());
+  app.setStyleSheet(style);
+}
+
+int main(int argc, char* argv[])
+{
+  QApplication app(argc, argv);
+  app.setStyle(QStyleFactory::create("Fusion"));
+  setPalette(app);
+
+  MainWindow window;
+  window.show();
+
+  return app.exec();
 }
