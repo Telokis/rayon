99bc14f0bb53ac3169ad6c26d37ec1241b1d2725
Telokis <telokis0@gmail.com>
2020-07-16 12:47:49 +0200

    Added run command to cmd scrip

--- cmd.sh
+++ cmd.sh
@@ -11,7 +11,9 @@ elif [ "$1" = "debug" ]; then
     run sw -config d --build-name rayon build -output-dir .sw/out/debug
 elif [ "$1" = "compdb" ]; then
     run sw --build-name rayon generate -g compdb
+elif [ "$1" = "run" ]; then
+    ./bin/rayon.$2-0.0.1.exe
 else
     echo "Command not found: $1"
-    echo "Try build, debug or compdb"
+    echo "Try build, debug, run or compdb"
 fi
