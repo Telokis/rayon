1416e3fbf9e8918dacac5f087bd7ffcf4ff2c7b5
Telokis <telokis0@gmail.com>
2021-08-26 19:55:31 +0200

    Improved cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -6,16 +6,20 @@ run() {
 }
 
 if [ "$1" = "build" ]; then
-    run sw -config r --build-name rayon build -output-dir bin;
+    if [ "$#" -eq 1 ]; then
+        run sw -config r --build-name rayon build -output-dir bin
+    else
+        run sw -config r --build-name rayon build -output-dir bin -target rayon.$2-0.0.1
+    fi
 elif [ "$1" = "debug" ]; then
     run sw -config d --build-name rayon build -output-dir .sw/out/debug
 elif [ "$1" = "compdb" ]; then
     run sw --build-name rayon generate -g compdb
 elif [ "$1" = "run" ]; then
     if [ "$2" = "cli" ]; then
-        ./bin/rayon.$2-0.0.1.exe  "-w" "1024" "-j" "8" "-h" "1024" "-o" "./out.png" "./files/scenes/above.yon"
+        run ./bin/rayon.$2-0.0.1.exe  "-w" "512" "-j" "2" "-h" "512" "-o" "./out.png" "./files/scenes/sunset.yon"
     else
-        ./bin/rayon.$2-0.0.1.exe $*
+        run ./bin/rayon.$2-0.0.1.exe "${@:3}"
     fi
 else
     echo "Command not found: $1"
